99322209885a9739a41803531195b9de0aa65325
Richard E. Nutsugah <61556574+eli-bigman@users.noreply.github.com>
2023-06-07 13:44:18 +0000

    Added UI Elements

---
+++ redApp/app/src/main/java/com/example/redapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.redapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button zero;
+    //control_btn
+    private Button AC;
+    private Button Del;
+    private Button percentage_btn;
+    private Button div_btn;
+    private Button mul_btn;
+    private Button add_btn;
+    private Button sub_btn;
+    private Button equal_btn;
+    private Button period_btn;
+    private Button ;
+
+
+
+}
